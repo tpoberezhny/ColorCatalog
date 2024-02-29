7a7a60d3519bdbf19469cf28ae34249279bec4fc
Tymofii Poberezhnyi <tymofii.poberezhnyi@publicisgroupe.cz>
2024-02-29 13:58:39 +0100

    Using AsyncStorage, adding hooks.js file

--- App.js
+++ App.js
@@ -2,18 +2,12 @@ import React, { useState } from "react";
 import { StyleSheet, FlatList } from "react-native";
 import ColorButton from "./components/ColorButton";
 import defaultColors from "./data/defaultColors.json";
-import { generate } from "shortid";
+import { useColors } from "./hooks";
+
 
 import ColorForm from "./components/ColorForm";
 
-const useColors = () => {
-  const [colors, setColors] = useState([]);
-  const addColor = (color) => {
-    const newColor = { id: generate(), color };
-    setColors([newColor, ...colors]);
-  };
-  return { colors, addColor}
-};
+
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
---
+++ hooks.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { generate } from "shortid";
+import  AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useColors = () => {
+  const [colors, setColors] = useState([]);
+
+  const loadColors = async () => {
+    const colorData = await AsyncStorage.getItem("@ColorListStore:Colors");
+    if (colorData) {
+      const colors = JSON.parse(colorData);
+      setColors(colors);
+    }
+  };
+
+  //Load colors
+  useEffect(() => {
+    if (colors.length) return;
+    loadColors();
+  }, []);
+
+  //Save colors
+  useEffect(() => {
+    AsyncStorage.setItem("@ColorListStore:Colors", JSON.stringify(colors));
+  }, [colors]);
+
+  const addColor = (color) => {
+    const newColor = { id: generate(), color };
+    setColors([newColor, ...colors]);
+  };
+  return { colors, addColor };
+};
